a357e8ab02e585d3a814d6cfd8a867f4e1f4d22b
Melissa Morel <melmorel06@gmail.com>
2017-01-27 10:41:05 -0800

    added simple instructions & place for user to type their question

--- src/components/CardBack.js
+++ src/components/CardBack.js
@@ -10,7 +10,7 @@ export default class CardBack extends Component {
   render() {
     return (
       <div className="Card">
-        <img src={this.imageSource()} />
+        <img src={this.imageSource()} alt="Back of a Card" />
       </div>
     )
   }
